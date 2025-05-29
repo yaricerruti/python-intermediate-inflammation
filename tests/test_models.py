6d4bd59c635aa010368037f313e685aff0876b55
Yari Cerruti <yari.cerruti@gmail.com>
2025-05-29 15:37:10 +0100

    combines mean functions tests into one

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,29 +6,41 @@ import pytest
 
 from inflammation.models import daily_mean, daily_max, daily_min
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
+@pytest.mark.parametrize( ## decorator
+        "test, expected",
+        [
+            ([[0,0], [0,0], [0,0]], [0,0]),
+            ([[1,2], [3,4], [5,6]], [3,4])
+        ]
+)
+
+def test_daily_mean(test, expected):
+    '''test mean function works for array of zeroes and positive integers'''
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
+# def test_daily_mean_zeros():
+#     """Test that mean function works for an array of zeros."""
     
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
+#     test_input = np.array([[0, 0],
+#                            [0, 0],
+#                            [0, 0]])
+#     test_result = np.array([0, 0])
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+#     # Need to use Numpy testing functions to compare arrays
+#     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
+# def test_daily_mean_integers():
+#     """Test that mean function works for an array of positive integers."""
 
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
+#     test_input = np.array([[1, 2],
+#                            [3, 4],
+#                            [5, 6]])
+#     test_result = np.array([3, 4])
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+#     # Need to use Numpy testing functions to compare arrays
+#     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 
 def test_daily_max():
